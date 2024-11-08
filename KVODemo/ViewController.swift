51e8e765c125ab0b3e27697ffc4c9a47ad51ead2
张亚飞 <672233580@qq.com>
2024-11-08 09:30:39 +0800

    update

--- KVODemo/ViewController.swift
+++ KVODemo/ViewController.swift
@@ -19,6 +19,15 @@ class ViewController: UIViewController {
         
         view.addSubview(btn)
         
+        //通过KVC设置Value能否生效
+        //可以 调用了set方法 set方法已经被动态生成子类重写了  可以出发KVO
+        person.setValue("bda", forKey: "name")
+        
+        //通过成员变量的直接复制 value能否生效
+        //不生效
+        person.changeName()
+        //手动KVO
+        person.changeName2()
     }
     
     
@@ -46,57 +55,28 @@ class ViewController: UIViewController {
             }
         }
     }
+    
+    
+    deinit {
+        person.removeObserver(self, forKeyPath: #keyPath(Person.name))
+    }
 
 
 }
 
 class Person: NSObject {
     @objc dynamic var name: String = ""
+    
+    func changeName() {
+        name = "changeName"
+    }
+    
+    /// 直接为成员变量赋值
+    func changeName2() {
+        willChangeValue(forKey: "name")
+        name = "changeName2"
+        didChangeValue(forKey: "name")
+    }
 }
 
-//class ViewController: UIViewController {
-//    var person: Person = Person()
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        // 确保添加观察者使用正确的 key path
-//        person.addObserver(self, forKeyPath: #keyPath(Person.name), options: [.new, .old], context: nil)
-//        person.name = "John"  // 触发 KVO
-//
-//        view.addSubview(btn)
-//    }
-//
-//    lazy var btn: UIButton = {
-//        let r = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
-//        r.backgroundColor = .red
-//        r.addTarget(self, action: #selector(changeName), for: .touchUpInside)
-//        return r
-//    }()
-//
-//    @objc func changeName() {
-//        print(#line, person.name)  // 输出当前 name
-//        person.name = "abc"       // 触发 KVO
-//    }
-//
-//    // 正确实现 KVO 观察者方法
-//    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-//        if keyPath == #keyPath(Person.name) {
-//            if let newName = change?[.newKey] as? String {
-//                print("New name: \(newName)")
-//            }
-//
-//            if let oldName = change?[.oldKey] as? String {
-//                print("Old name: \(oldName)")
-//            }
-//        }
-//    }
-//
-//    deinit {
-//        person.removeObserver(self, forKeyPath: #keyPath(Person.name))
-//    }
-//}
-//
-//class Person: NSObject {
-//    @objc dynamic var name: String = ""
-//}
+
